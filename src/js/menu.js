d8bf73e1ac9ce831d8eaf8a197d937f6ed56c824
Arnaut898 <79265309+Arnaut898@users.noreply.github.com>
2021-06-14 22:32:11 +0500

    Delete src directory

--- src/js/menu.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('.menu__button');
-const menu = document.querySelector('.menu')
-
-menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('menu__button-active')
-    menu.classList.toggle('menu-active')
-})
